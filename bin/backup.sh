5ff277deeaa2f03ab2a6134dda71b0b69cf8aabd
floflodu59 <florent.fleuri@infosyscom.eu>
2025-05-13 16:43:52 +0200

    Update backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -65,11 +65,13 @@ exec 3>&1
 bckcfg=$(dialog --ok-label "Continuer" \
 	--title "CONFIGURATION SAUVEGARDES" \
 	--form "Entrez la configuration de la sauvegarde :" \
-15 80 0 \
+20 80 0 \
 	"Adresse mail de l'envoyeur :"	1 1	"$sender" 		1 40 40 0 \
 	"Adresse mail du destinataire :"	2 1	"$recipient" 		2 40 40 0 \
 	"Nom de l'entreprise :"	3 1	"$sujet" 		3 40 40 0 \
 	"Nom du site :"	4 1	"$site" 		4 40 40 0 \
+	"Utilisateur SMTP2GO :"	5 1	"$user" 		5 40 40 0 \
+	"Mot de passe SMTP2GO :"	6 1	"$pwd" 		6 40 40 0 \
 2>&1 1>&3)
 exec 3>&-
 IFS=$'\n'; bckarray=($bckcfg); unset IFS;
@@ -77,9 +79,16 @@ sender=${bckarray[0]}
 recipient=${bckarray[1]}
 sujet=${bckarray[2]}
 site=${bckarray[3]}
+user=${bckarray[4]}
+pwd=${bckarray[5]}
 sed -i 's/sender=""/sender="'$sender'"/g' /backup/scripts/errorhandler.sh
 sed -i 's/recipients=""/recipients="'$recipient'"/g' /backup/scripts/errorhandler.sh
 sed -i 's/site="IFF-01"/site="'$site'"/g' /backup/scripts/errorhandler.sh
 sed -i 's/sujet="ISC"/sujet="'$sujet'"/g' /backup/scripts/errorhandler.sh
+echo "mailhub=mail.smtp2go.com:2525" > /etc/ssmtp/ssmtp.conf
+echo "AuthUser=$user" >> /etc/ssmtp/ssmtp.conf
+echo "AuthPass=$pwd" >> /etc/ssmtp/ssmtp.conf
+echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf
+echo "FromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
 echo "VMISIL" >> /backup/scripts/vmlist
 mount -a
